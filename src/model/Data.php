b0e14726933feb00b77e1e7fb1d23e3ab3bdd5a0
xqkeji <xqkeji@foxmail.com>
2024-02-08 23:41:00 +0800

    更新Data

--- src/model/Data.php
+++ src/model/Data.php
@@ -1,9 +1,13 @@
 <?php
 namespace xqkeji\app\content\model;
 use xqkeji\mvc\builder\Model;
+use xqkeji\forms\element\Pager;
 use xqkeji\App;
 class Data
 {
+    /**
+     * 根据栏目的id获取栏目名称
+     */
     public static function getNameById(string $cat_id)
     {
         $category=Model::getModel('category','content');
@@ -15,11 +19,17 @@ class Data
         }
         return $name;
     }
+    /**
+     * 根据栏目名称从Reg组件里获取栏目id
+     */
     public static function getRegCatId(string $cat_name)
     {
         $reg=App::getReg();
         return $reg->get($cat_name.'_content_cat_id');
     }
+    /**
+     * 根据栏目名称获取内容列表
+     */
     public static function getByCatName(string $cat_name,int $limit=5,array $order=['create_time'=>'desc'])
     {
         $category=Model::getModel('category','content');
@@ -36,6 +46,9 @@ class Data
         }
         return $data;
     }
+    /**
+     * 根据栏目id获取内容列表
+     */
     public static function getByCatId(string $cat_id,int $limit=5,array $order=['create_time'=>'desc'])
     {
         $content=Model::getModel('content','content');
@@ -59,6 +72,9 @@ class Data
         }
         return $data;
     }
+    /**
+     * 根据栏目名称获取单页的内容信息
+     */
     public static function getPageByCatName(string $cat_name)
     {
         $category=Model::getModel('category','content');
@@ -77,6 +93,9 @@ class Data
         }
         return $data;
     }
+    /**
+     * 根据栏目id获取单页的内容信息
+     */
     public static function getPageByCatId(string $cat_id)
     {
         $page=Model::getModel('page','content');
@@ -94,5 +113,81 @@ class Data
             ];
         }
         return $data;
+    }
+    /**
+     * 根据栏目id获取内容的分页列表
+     */
+    public static function getListByCatId(string $cat_id,int $page_size=10,array $order=['create_time'=>'desc'])
+    {
+        $content=Model::getModel('content','content');
+        $total=$content->where(['cat_id'=>$cat_id,'status'=>1])->count();
+        $request=App::getRequest();
+        $requestData=$request->getQuery();
+        $pageParams=[];
+        $pageParams["name"]="listPager";
+		$pageParams["total"]=$total;
+		$pageParams["defaultPageSize"]=$page_size;
+		$pageParams["maxBtn"]=5;
+		$pageParams["params"]=$requestData;
+		$pager=new Pager($pageParams);
+        $rows=$content->where(['cat_id'=>$cat_id,'status'=>1])
+        ->limit($pager->getOffset(),$pager->getPageSize())
+        ->order($order)->select();
+        $data=[];
+        if($rows)
+        {
+            foreach($rows as $row)
+            {
+                $title=$row->getAttr('title');
+                $id=(string)$row->getKey();
+                $row=[
+                    'id'=>$id,
+                    'title'=>$title,
+                    'create_time'=>date('Y-m-d',$row->getAttr('create_time'))
+                ];
+                $data[]=$row;
+            }
+        }
+        return [
+            'pager'=>$pager,
+            'rows'=>$data,
+        ];
+
+    }
+    /**
+     * 根据内容id获取内容信息
+     */
+    public static function getContentById(string $id)
+    {
+        $content=Model::getModel('content','content');
+        $row=$content->find($id);
+        $data=[];
+        if($row)
+        {
+            $data=$row->toArray();
+        }
+        return $data;
+    }
+    /**
+     * 根据栏目id获取栏目信息
+     */
+    public static function getCategoryById(string $id)
+    {
+        $category=Model::getModel('category','content');
+        $row=$category->find($id);
+        $data=[];
+        if($row)
+        {
+            $data=$row->toArray();
+        }
+        return $data;
+    }
+    /**
+     * 根据栏目id获取栏目路径上的所有栏目链接
+     */
+    public static function getRootPath($cat_id)
+    {
+        $category=Model::getModel('category','content');
+        return $category->getRootPath($cat_id);
     }
 }
